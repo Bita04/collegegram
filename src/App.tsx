0d257a5644a1318a5afc242c951b3691aaa6087b
mamadsilex <mamadsilex7@gmail.com>
2023-08-22 19:21:16 +0330

    modify app.tsx and delete react svg's

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Layout } from "./components/Layout/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import { RtlProvider } from "./components/rtl-provider";
+import InputText from "./components/ui/input/inputText";
 const theme = extendTheme();
 function App() {
   return (
     <ChakraProvider>
       <RtlProvider>
-        <Layout />
+        <Layout>
+          <h1>Hello!</h1>
+        </Layout>
       </RtlProvider>
     </ChakraProvider>
   );
