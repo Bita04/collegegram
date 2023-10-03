cdaa4c8b80ccef20d25712a96bb80880dc921431
Bita04 <bita.bahadori.83@gmail.com>
2023-10-03 22:58:22 +0330

    the correct start

---
+++ src/components/MyUserExplore/EachUserExplore.tsx
@@ -0,0 +1,17 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+const EachUserExplore = () => {
+  return (
+    <div>
+      <Flex className="w-[100%] max-w-6xl flex-wrap bg-pink gap-[24px]">
+        <Flex className="w-[232px] flex-row h-[232px] bg-[#000]  rounded-[24px]"></Flex>
+        <Flex className="w-[232px] flex-row h-[232px] bg-[#000] rounded-[24px]"></Flex>
+        <Flex className="w-[232px] flex-row h-[232px] bg-[#000]  rounded-[24px]"></Flex>
+        <Flex className="w-[232px] flex-row h-[232px] bg-[#000]  rounded-[24px]"></Flex>
+      </Flex>
+    </div>
+  );
+};
+
+export default EachUserExplore;
