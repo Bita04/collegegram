500e1be7959bfa95dfdf61975cd3c97711e836e3
Bita04 <bita.bahadori.83@gmail.com>
2023-10-03 23:03:17 +0330

    ...

--- src/pages/MyExplore.tsx
+++ src/pages/MyExplore.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { UserProfile } from "./UserProfile";
-import MyUsersExplore from "../components/MyUsersExplore/MyUsersExplore";
+import MyUsersExplore from "../components/MyUserExplore/MyUserExplore";
 
 const MyExpolre = () => {
   return (
