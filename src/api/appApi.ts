f3a21a889422bf05084b64f1c01a34465dbd0587
aka-abdol <thegrolam@gmail.com>
2023-10-07 18:13:27 +0330

    fix: get user profile api done

--- src/api/appApi.ts
+++ src/api/appApi.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-
-const BASE_URL = 'https://collegegramgreedy.darkube.app';
+const BASE_URL = "https://collegegramgreedy.darkube.app";
 
 export const appApi = axios.create({
   baseURL: BASE_URL,
-
-
 });
 // axios.get("https://collegegram-greedy-test.darkube.app/post", formData, {
 //   headers: {
@@ -14,92 +11,67 @@ export const appApi = axios.create({
 //     "authorization": localStorage.getItem("accessToken"),
 //     "refresh-token" : localStorage.getItem("refreshToken")
 
-
-
-
 //   }
 // }).then((res) => {
 
 //   console.log(res);
 // })
 
-export const getPosts = async (limit: number, nextOffset:number, first: boolean) => 
-{
-  if(first) {
+export const getPosts = async (
+  limit: number,
+  nextOffset: number,
+  first: boolean
+) => {
+  if (first) {
     const response = await appApi.get(`/post/user?limit=${limit}`, {
-          headers: {
-            
-            "authorization": localStorage.getItem("accessToken"),
-            "refresh-token" : localStorage.getItem("refreshToken")
-    
-    
-        
-    
-          }
-        });
-        return response.data
-    
-
-  }
-  else {
-    const response = await appApi.post(`/post/user?limit=${limit}&startTime=${new Date(nextOffset).getTime()}`);
+      headers: {
+        authorization: localStorage.getItem("accessToken"),
+        "refresh-token": localStorage.getItem("refreshToken"),
+      },
+    });
     return response.data;
-
+  } else {
+    const response = await appApi.post(
+      `/post/user?limit=${limit}&startTime=${new Date(nextOffset).getTime()}`
+    );
+    return response.data;
   }
-  
+
   // console.log(new Date(nextOffset).getTime())
   // if(nextOffset === '0') {
   //   const response = await appApi.get(`/post/user?limit=${limit}`, {
   //     headers: {
-        
+
   //       "authorization": localStorage.getItem("accessToken"),
   //       "refresh-token" : localStorage.getItem("refreshToken")
-
-
-    
 
   //     }
   //   });
   //   return response.data;
-    
 
   // }
   // else{
-    
 
   // }
-    
-  };
+};
 
-  export const getByUserName = async (username: string) => {
-    // console.log(username)
-   const response = await appApi.post("/user/getUserProfile", {
-      userName : username
-    }, {
-  headers: {
-    "authorization": localStorage.getItem("accessToken"),
-    "refresh-token" : localStorage.getItem("refreshToken")
+export const getByUserName = async (username: string) => {
+  // console.log(username)
+  const response = await appApi.get(`/user/getUserProfile/${username}`, {
+    headers: {
+      authorization: localStorage.getItem("accessToken"),
+      "refresh-token": localStorage.getItem("refreshToken"),
+    },
+  });
+  return response.data;
+};
 
-
-
-
-  }
-})
-return response.data
-
-  }
-
-  export const getInfoUser = async () => {
-    const response = await appApi.get("/user/getUser",  {
-   headers: {
-     "authorization": localStorage.getItem("accessToken"),
-     "refresh-token" : localStorage.getItem("refreshToken")
- 
- 
- 
- 
-   }
- })
- return response.data
- 
-   }
+export const getInfoUser = async () => {
+  const response = await appApi.get("/user/getUser", {
+    headers: {
+      authorization: localStorage.getItem("accessToken"),
+      "refresh-token": localStorage.getItem("refreshToken"),
+    },
+  });
+  return response.data;
+};
